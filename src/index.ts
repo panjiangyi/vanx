6c5feac5a3404d2462fed276621afa9f5842652e
panjiangyi <78237724@qq.com>
2021-06-13 15:19:52 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,45 @@
-export * from './lib/number';
+
+type Updator = (v: unknown, vName: string) => void;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type VueType = any
+const noop = () => null;
+export default class StoreCore {
+    protected proxy(this: StoreCore) {
+        Object.keys(this).filter(key => !["eventCenter", "decorator"].includes(key)).forEach((key: string) => {
+            let value: unknown = this[key as keyof StoreCore];
+            Object.defineProperty(this, key, {
+                set: (v: unknown) => {
+                    value = v;
+                    this.eventCenter.forEach(func => func(value, key));
+                },
+                get: () => {
+                    return value;
+                }
+            })
+        })
+    }
+    protected eventCenter: Array<Updator> = []
+    public decorator(wantedVariable: string) {
+        const store = this;
+        return function (target: VueType, name: string) {
+            target[name] = store[wantedVariable as keyof StoreCore];
+            const oldCreated = target.created || noop;
+            const oldBeforeDestroy = target.beforeDestroy || noop;
+
+            let updator: Updator
+            target.created = function (this: VueType, ...params: readonly unknown[]) {
+                updator = (v: unknown, vName: string) => {
+                    if (vName !== wantedVariable) return;
+                    this[name] = v
+                }
+                store.eventCenter.push(updator)
+                return oldCreated.apply(this, params);
+            }
+            target.beforeDestroy = function (this: VueType, ...params: readonly unknown[]) {
+                store.eventCenter = store.eventCenter.filter(k => k !== updator);
+                return oldBeforeDestroy.apply(this, params);
+            }
+            return target
+        };
+    }
+}
